fbe91a8e46d352f2239a6357152a95ed0e4b8081
zersh <zersh@mail.ru>
2016-04-15 12:31:53 +0300

    Integrete whith ChocoMon

--- Chocolatey_chocomaint/tools/chocolateyInstall.ps1
+++ Chocolatey_chocomaint/tools/chocolateyInstall.ps1
@@ -1,9 +1,10 @@
 ﻿$packageName = 'chocomaint'
 $installerType = 'EXE' 
-$url = 'https://raw.githubusercontent.com/zersh01/ChocoMain/master/InstallChocoMaint/ChocoMaint_1.0.3.5_Setup.exe'
+$url = 'https://raw.githubusercontent.com/zersh01/ChocoMain/master/InstallChocoMaint/ChocoMaint_1.0.3.6_Setup.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0) 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"   -validExitCodes $validExitCodes
+
 
 
 
